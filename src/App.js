34f9b4cd2358d29e1682d8f23678e0da5970f13e
miloszczapla <miloszczapla@gmail.com>
2022-04-14 23:27:46 +0200

    Animated slider with fckBHC queto :)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
+import { Header } from './components/Header';
+import { FckBHC } from './components/FckBHC';
+
 export function App() {
-  return <div>hey</div>;
+  return (
+    <>
+      <FckBHC />
+      <Header />
+      <img src='' alt='' />
+      <h2 className='text-center '>
+        <span className='uppercase text-blue'>w</span>
+        <span className='uppercase text-crimson'>b</span>
+        <span className='uppercase text-light-violet'>i</span>
+        <span className='uppercase text-crimson'>j</span>
+        <span className='uppercase text-violet'>a</span>
+        <span className='uppercase text-blue'>m</span> się w{' '}
+        <spam className='text-red shadow-text-red'>BIT</spam>a na{' '}
+        <span className=' text-violet text-shadow-violet'>HAMa</span>
+      </h2>
+    </>
+  );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Header() {
+  return (
+    <header className='text-center text-7xl font-comfortaa text-red text-shadow-red p-8'>
+      To jest odpowiedź na ryk świni!!!
+    </header>
+  );
+}
